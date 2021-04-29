383daacbb88e089eac3b6de7012496191ef5f34c
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2021-04-29 11:55:11 -0700

    Update plane_averaged_density.py
    added method to calculate vacuum potential

--- charge_density_methods_VASP/plane_averaged_density.py
+++ charge_density_methods_VASP/plane_averaged_density.py
@@ -1,7 +1,26 @@
-from numpy import array, shape, zeros
+from numpy import array, shape, zeros, argmin
+from numpy.linalg import norm
 import matplotlib.pyplot as plt
 
 from lib import parse_CHGCAR, parse_LOCPOT
+
+def find_vacuum_potential(ifile,**args):
+    if 'dim' in args:
+        dim=args['dim']
+    else:
+        dim=2
+        
+    x,y,e,lv,coord=calc_plane_averaged_density(ifile,dim=dim)
+    
+    mindiff=array([norm(lv[dim]) for i in range(shape(e)[dim])])
+    for i in range(shape(e)[dim]):
+        for j in coord[:,dim]:
+            for k in range(3):
+                if mindiff[i]>abs(x[i]-j+norm(lv[dim])*k):
+                    mindiff[i]=abs(x[i]-j+norm(lv[dim])*k)
+    
+    min_index=argmin(mindiff)
+    return y[min_index]
 
 def calc_plane_averaged_density(ifile,**args):
     if 'dim' in args:
@@ -19,17 +38,18 @@ def calc_plane_averaged_density(ifile,**args):
     else:
         e,lv,coord,atomtypes,atomnums=parse_CHGCAR(ifile)
     
-    x=array([i*lv/(shape(e)[dim]-1) for i in range(shape(e)[dim])])
+    x=array([i*norm(lv[dim])/(shape(e)[dim]-1) for i in range(shape(e)[dim])])
     y=zeros(shape(e)[dim])
     for i in range(shape(e)[dim]):
         y[i]+=sum(e[:,:,i].flatten())
         for j in range(3):
             if j!=dim:
-                y[i]/=shape(e)[dim]
+                y[i]/=shape(e)[j]
                 
-    return x,y
+    return x,y,e,lv,coord
 
-def plot_plane_averaged_density(x,y):
+def plot_plane_averaged_density(ifile):
+    x,y=calc_plane_averaged_density(ifile)[:2]
     plt.figure()
     plt.plot(x,y)
     plt.ylabel('electrostatice potential / eV')
