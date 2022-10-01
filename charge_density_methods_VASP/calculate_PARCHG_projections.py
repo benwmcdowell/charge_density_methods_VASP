b2a5272ecc3118698ed71a036c11dc7d8dbfdd3b
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2022-09-30 17:59:21 -0700

    Update calculate_PARCHG_projections.py
    Normalized calculation of charge density projections

--- charge_density_methods_VASP/calculate_PARCHG_projections.py
+++ charge_density_methods_VASP/calculate_PARCHG_projections.py
@@ -8,11 +8,16 @@ def calc_PARCHG_projections(ref,fp):
     listdir=os.listdir(fp)
     eref=parse_CHGCAR(ref)[0]
     bands=[]
-    overlap=[]
+    overlaps=[]
     for i in listdir:
         if 'PARCHG' in i:
             bands.append(int(i.split('.')[1])-1)
             etemp=parse_CHGCAR(i)[0]
-            overlap.append(np.dot(eref,etemp)/np.linalg.norm(eref)/np.linalg.norm(etemp))
+            overlaps.append(np.linalg.norm(eref*etemp)/np.linalg.norm(etemp))
             
-    return bands,overlap
+    overlaps=np.array(overlaps)
+    overlaps-=np.min(overlaps)
+    overlaps/=np.max(overlaps)
+    bands=np.array(bands)        
+    
+    return bands,overlaps
