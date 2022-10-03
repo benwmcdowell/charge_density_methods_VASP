bfd51845b164d20ef7e814cf3d4a3c00429654f6
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2022-10-03 15:45:26 -0700

    Update calculate_PARCHG_projections.py
    Added method to read and return eigenenergy from filename

--- charge_density_methods_VASP/calculate_PARCHG_projections.py
+++ charge_density_methods_VASP/calculate_PARCHG_projections.py
@@ -12,12 +12,14 @@ def calc_PARCHG_projections(ref,fp):
     overlaps=[]
     average_zpos=[]
     zmask=np.zeros(npts)
+    energies=[]
     for i in range(npts[2]):
         zmask[:,:,i]=i/(npts[2]-1)*np.linalg.norm(lv[2])
     for i in listdir:
         if 'PARCHG' in i:
             bands.append(int(i.split('.')[1])-1)
             etemp=parse_CHGCAR(i)[0]
+            energies.append(float('.'.join(i.split('.')[-2:])))
             overlaps.append(np.linalg.norm(eref*etemp)/np.linalg.norm(etemp))
             average_zpos.append(np.linalg.norm(zmask*etemp/np.linalg.norm(etemp)))
             
@@ -26,4 +28,4 @@ def calc_PARCHG_projections(ref,fp):
     overlaps/=np.max(overlaps)
     bands=np.array(bands)        
     
-    return bands,overlaps
+    return bands,overlaps,average_zpos,energies
