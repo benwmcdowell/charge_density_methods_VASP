4b76f7d51e97db74cc0283f407887a5f5e285015
Benjamin McDowell <71850608+benwmcdowell@users.noreply.github.com>
2023-03-08 15:57:47 -0800

    Update path_slice.py
    Modified to use class structure, reducing computational cost of loading density files

--- charge_density_methods_VASP/path_slice.py
+++ charge_density_methods_VASP/path_slice.py
@@ -14,200 +14,197 @@ from lib import parse_CHGCAR, parse_LOCPOT, parse_poscar
 #path_atom indices count from 1, as displayed in VESTA
 #if specified, read_data_from_file allows the density data to be read from a .npy file
 #if read_data_from_file is used, ifile should be the path to the directory of the POSCAR/CONTCAR
-def slice_path(ifile,path_atoms,read_data_from_file=False,**args):
-    if 'filetype' in args:
-        filetype=args['filetype']
-    else:
-        filetype='CHGCAR'
-    
-    if filetype=='LOCPOT' and not read_data_from_file:
-        e,lv,coord,atomtypes,atomnums=parse_LOCPOT(ifile)
-    elif 'CHG' in filetype and not read_data_from_file:
-        e,lv,coord,atomtypes,atomnums=parse_CHGCAR(ifile)
-    elif filetype=='none':
-        npts=1000
-        lv,coord,atomtypes,atomnums=parse_poscar(ifile)[:4]
-        e=np.zeros((npts,npts,npts))
-    if read_data_from_file:
-        e=np.load(read_data_from_file)
-        os.chdir(ifile)
-        try:
-            lv,coord,atomtypes,atomnums=parse_poscar('./CONTCAR')[:4]
-        except FileNotFoundError:
-            lv,coord,atomtypes,atomnums=parse_poscar('./POSCAR')[:4]
+class slice_path():
+    def __init__(self,ifile,path_atoms,read_data_from_file=False,filetype='CHGCAR',cmap='seismic',**args):
+        self.path_atoms=path_atoms
+        self.filetype=filetype
+        self.cmap=cmap
+        if self.filetype=='LOCPOT' and not read_data_from_file:
+            self.e,self.lv,self.coord,self.atomtypes,self.atomnums=parse_LOCPOT(ifile)
+        elif 'CHG' in self.filetype and not read_data_from_file:
+            self.e,self.lv,self.coord,self.atomtypes,self.atomnums=parse_CHGCAR(ifile)
+        elif self.filetype=='none':
+            npts=1000
+            self.lv,self.coord,self.atomtypes,self.atomnums=parse_poscar(ifile)[:4]
+            self.e=np.zeros((npts,npts,npts))
+        if read_data_from_file:
+            self.e=np.load(read_data_from_file)
+            os.chdir(ifile)
+            try:
+                self.lv,self.coord,self.atomtypes,self.atomnums=parse_poscar('./CONTCAR')[:4]
+            except FileNotFoundError:
+                self.lv,self.coord,self.atomtypes,self.atomnums=parse_poscar('./POSCAR')[:4]
+            
+        dim=np.shape(self.e)
         
-    dim=np.shape(e)
-    
-    if 'ref' in args:
-        for i in args['ref']:
-            if filetype=='LOCPOT':
-                tempvar=parse_LOCPOT(i)[0]
+        if 'ref' in args:
+            for i in args['ref']:
+                if self.filetype=='LOCPOT':
+                    tempvar=parse_LOCPOT(i)[0]
+                else:
+                    tempvar=parse_CHGCAR(i)[0]
+                self.e-=tempvar
+                
+        if 'norm' in args:
+            norm_mode=args['norm']
+            if norm_mode not in ['none','slice','total']:
+                print('unknown normalization prompt. data will not be normalized.')
+        else:
+            norm_mode='none'
+                
+        if 'gradient' in args:
+            if args['gradient']==True:
+                de=np.zeros(dim)
+                dr=[np.linalg.norm(self.lv[i])/dim[i] for i in range(3)]
+                for i in range(dim[0]):
+                    for j in range(dim[1]):
+                        for k in range(dim[2]):
+                            for l in [-1,1]:
+                                if i+l>=dim[0]:
+                                    de[i][j][k]+=abs((self.e[i+l-dim[0]][j][k]-self.e[i+l-dim[0]-1][j][k])/dr[0]/2.0)
+                                else:
+                                    de[i][j][k]+=abs((self.e[i+l][j][k]-self.e[i+l-1][j][k])/dr[0]/2.0)
+                                if j+l>=dim[1]:
+                                    de[i][j][k]+=abs((self.e[i][j+l-dim[1]][k]-self.e[i][j+l-dim[1]-1][k])/dr[1]/2.0)
+                                else:
+                                    de[i][j][k]+=abs((self.e[i][j+l][k]-self.e[i][j+l-1][k])/dr[1]/2.0)
+                                if k+l>=dim[2]:
+                                    de[i][j][k]+=abs((self.e[i][j][k+l-dim[2]]-self.e[i][j][k+l-dim[2]-1])/dr[2]/2.0)
+                                else:
+                                    de[i][j][k]+=abs((self.e[i][j][k+l]-self.e[i][j][k+l-1])/dr[2]/2.0)
+                self.e=de
+                print('density gradient calculated.')
+                            
+        if 'zrange' in args:
+            self.zrange=args['zrange']
+        else:
+            self.zrange=[0.0,1.0]
+            
+        if 'tol' in args:
+            self.tol=args['tol']
+        else:
+            self.tol=0.0
+        
+        if 'colors' in args:
+            self.colors=args['colors']
+        else:
+            self.colors=['black' for i in range(len(self.atomnums))]
+            
+        if 'sizes' in args:
+            self.sizes=args['sizes']
+        else:
+            self.sizes=[100 for i in range(len(self.atomnums))]
+            
+        if 'direct' in args:
+            for i in range(2):
+                self.zrange[i]=np.dot(self.zrange[i],self.lv[2])
+        
+        self.path=[]
+        for i in self.path_atoms:
+            if len(i)>1:
+                tempvar=i[1:]
             else:
-                tempvar=parse_CHGCAR(i)[0]
-            e-=tempvar
+                tempvar=[0,0]
+            self.path.append(deepcopy(self.coord[i[0]-1,:2]))
+            for j in range(2):
+                self.path[-1]+=self.lv[j,:2]*float(tempvar[j])
+                
+        #adds tolerance to the initial and final positions specified by the path
+        idiff=(self.path[1]-self.path[0])/np.linalg.norm(self.path[1]-self.path[0])
+        fdiff=(self.path[-1]-self.path[-2])/np.linalg.norm(self.path[-1]-self.path[-2])
+        self.path[0]-=idiff*self.tol
+        self.path[-1]+=fdiff*self.tol
             
-    if 'norm' in args:
-        norm_mode=args['norm']
-        if norm_mode not in ['none','slice','total']:
-            print('unknown normalization prompt. data will not be normalized.')
-    else:
-        norm_mode='none'
+        path_length=sum([np.linalg.norm(self.path[i]-self.path[i-1]) for i in range(1,len(self.path))])
+        
+        if 'npts' in args:
+            self.npts=args['npts']
+        else:
+            self.npts=path_length/min([np.linalg.norm(self.lv[j]) for j in range(3)])*min(dim)
             
-    if 'gradient' in args:
-        if args['gradient']==True:
-            de=np.zeros(dim)
-            dr=[np.linalg.norm(lv[i])/dim[i] for i in range(3)]
-            for i in range(dim[0]):
-                for j in range(dim[1]):
-                    for k in range(dim[2]):
-                        for l in [-1,1]:
-                            if i+l>=dim[0]:
-                                de[i][j][k]+=abs((e[i+l-dim[0]][j][k]-e[i+l-dim[0]-1][j][k])/dr[0]/2.0)
-                            else:
-                                de[i][j][k]+=abs((e[i+l][j][k]-e[i+l-1][j][k])/dr[0]/2.0)
-                            if j+l>=dim[1]:
-                                de[i][j][k]+=abs((e[i][j+l-dim[1]][k]-e[i][j+l-dim[1]-1][k])/dr[1]/2.0)
-                            else:
-                                de[i][j][k]+=abs((e[i][j+l][k]-e[i][j+l-1][k])/dr[1]/2.0)
-                            if k+l>=dim[2]:
-                                de[i][j][k]+=abs((e[i][j][k+l-dim[2]]-e[i][j][k+l-dim[2]-1])/dr[2]/2.0)
-                            else:
-                                de[i][j][k]+=abs((e[i][j][k+l]-e[i][j][k+l-1])/dr[2]/2.0)
-            e=de
-            print('density gradient calculated.')
-                        
-    if 'zrange' in args:
-        zrange=args['zrange']
-    else:
-        zrange=[0.0,1.0]
+        step_points=np.array([round(np.linalg.norm(self.path[i]-self.path[i-1])/path_length*self.npts)-1 for i in range(1,len(self.path))])
+        step_points[0]+=1
+        self.npts=sum(step_points)
+        path_distance=np.array([path_length*i/(self.npts-1) for i in range(self.npts)])
+        self.path_coord=[self.path[0]]
+        for i in range(1,len(self.path)):
+            for j in range(step_points[i-1]):
+                if i==1 and j==0:
+                    pass
+                else:
+                    self.path_coord.append(self.path[i-1]+(self.path[i]-self.path[i-1])/(step_points[i-1]-1)*j)
+        self.path_coord=np.array(self.path_coord)
         
-    if 'cmap' in args:
-        cmap=args['cmap']
-    else:
-        cmap='seismic'
+        for i in range(len(self.path_coord)):
+            self.path_coord[i]=np.dot(self.path_coord[i],np.linalg.inv(self.lv[:2,:2]))
+            for j in range(2):
+                while self.path_coord[i][j]>=1.0 or self.path_coord[i][j]<0.0:
+                    if self.path_coord[i][j]>=1.0:
+                        self.path_coord[i][j]-=1.0
+                    if self.path_coord[i][j]<0.0:
+                        self.path_coord[i][j]+=1.0
+                self.path_coord[i][j]=int(floor(self.path_coord[i][j]*dim[j]))
         
-    if 'tol' in args:
-        tol=args['tol']
-    else:
-        tol=0.0
-    
-    if 'colors' in args:
-        colors=args['colors']
-    else:
-        colors=['black' for i in range(len(atomnums))]
-        
-    if 'sizes' in args:
-        sizes=args['sizes']
-    else:
-        sizes=[800 for i in range(len(atomnums))]
-        
-    if 'direct' in args:
         for i in range(2):
-            zrange[i]=np.dot(zrange[i],lv[2])
-    
-    path=[]
-    for i in path_atoms:
-        if len(i)>1:
-            tempvar=i[1:]
+            self.zrange[i]=round(self.zrange[i]*dim[2])
+        self.z=np.zeros((self.npts,self.zrange[1]-self.zrange[0]))
+        for i in range(self.npts):
+            self.z[i]=self.e[int(self.path_coord[i][0]),int(self.path_coord[i][1]),self.zrange[0]:self.zrange[1]]
+        if norm_mode=='slice':
+            self.z-=np.min(self.z)
+            self.z/=np.max(self.z)
+        elif norm_mode=='total':
+            self.z-=np.min(self.e)
+            self.z/=(np.max(self.e)-np.min(self.e))
+        
+        self.x=np.array([path_distance for i in range(self.zrange[1]-self.zrange[0])]).transpose()
+        self.y=np.array([[(self.zrange[1]-self.zrange[0])/dim[2]*np.linalg.norm(self.lv[2])*j/dim[2] for i in range(self.npts)] for j in range(self.zrange[1]-self.zrange[0])]).transpose()
+        
+        self.plot_main()
+        
+    def plot_main(self,**args):
+        self.fig_main,self.ax_main=plt.subplots(1,1)
+        self.density_plot=self.ax_main.pcolormesh(self.x,self.y,self.z,cmap=self.cmap,shading='nearest')
+        cbar=self.fig_main.colorbar(self.density_plot)
+        if 'clim' not in args:
+            max_val=max([abs(i) for i in self.z.flatten()])
+            self.density_plot.set_clim(vmin=-max_val,vmax=max_val)
         else:
-            tempvar=[0,0]
-        path.append(deepcopy(coord[i[0]-1,:2]))
-        for j in range(2):
-            path[-1]+=lv[j,:2]*float(tempvar[j])
+            self.density_plot.set_clim(vmin=args['clim'][0],vmax=args['clim'][1])
+        if 'CHG' in self.filetype:
+            cbar.set_label('change in electron density / electrons $\AA^{-3}$')
+        if self.filetype=='LOCPOT':
+            cbar.set_label('electrostatic potential / eV')
+            cbar.ax.yaxis.set_major_formatter(FormatStrFormatter('%+.4f'))
+        atom_pos=[self.tol]
+        for i in range(1,len(self.path)):
+            atom_pos.append(atom_pos[-1]+np.linalg.norm(self.path[i]-self.path[i-1]))
+            if i==1:
+                atom_pos[i]-=self.tol
+            if i==len(self.path)-1:
+                atom_pos[i]-=self.tol
+        for i in range(len(self.path_atoms)):
+            for j in range(len(self.atomtypes)):
+                if self.path_atoms[i][0]-1 < sum(self.atomnums[:j+1]):
+                    break
+            self.ax_main.scatter(atom_pos[i],self.coord[self.path_atoms[i][0]-1][2]-self.zrange[0],color=self.colors[j],s=self.sizes[j])
             
-    #adds tolerance to the initial and final positions specified by the path
-    idiff=(path[1]-path[0])/norm(path[1]-path[0])
-    fdiff=(path[-1]-path[-2])/norm(path[-1]-path[-2])
-    path[0]-=idiff*tol
-    path[-1]+=fdiff*tol
-        
-    path_length=sum([norm(path[i]-path[i-1]) for i in range(1,len(path))])
-    
-    if 'npts' in args:
-        npts=args['npts']
-    else:
-        npts=path_length/min([norm(lv[j]) for j in range(3)])*min(dim)
-        
-    step_points=np.array([round(np.linalg.norm(path[i]-path[i-1])/path_length*npts)-1 for i in range(1,len(path))])
-    step_points[0]+=1
-    npts=sum(step_points)
-    path_distance=np.array([path_length*i/(npts-1) for i in range(npts)])
-    path_coord=[path[0]]
-    for i in range(1,len(path)):
-        for j in range(step_points[i-1]):
-            if i==1 and j==0:
-                pass
+        if 'contour' in args:
+            if self.filetype=='LOCPOT':
+                tempvar=parse_LOCPOT(args['contour'][0])[0]
             else:
-                path_coord.append(path[i-1]+(path[i]-path[i-1])/(step_points[i-1]-1)*j)
-    path_coord=np.array(path_coord)
-    
-    for i in range(len(path_coord)):
-        path_coord[i]=np.dot(path_coord[i],np.linalg.inv(lv[:2,:2]))
-        for j in range(2):
-            while path_coord[i][j]>=1.0 or path_coord[i][j]<0.0:
-                if path_coord[i][j]>=1.0:
-                    path_coord[i][j]-=1.0
-                if path_coord[i][j]<0.0:
-                    path_coord[i][j]+=1.0
-            path_coord[i][j]=int(floor(path_coord[i][j]*dim[j]))
-    
-    for i in range(2):
-        zrange[i]=round(zrange[i]*dim[2])
-    z=np.zeros((npts,zrange[1]-zrange[0]))
-    for i in range(npts):
-        z[i]=e[int(path_coord[i][0]),int(path_coord[i][1]),zrange[0]:zrange[1]]
-    if norm_mode=='slice':
-        z-=np.min(z)
-        z/=np.max(z)
-    elif norm_mode=='total':
-        z-=np.min(e)
-        z/=(np.max(e)-np.min(e))
-    
-    x=np.array([path_distance for i in range(zrange[1]-zrange[0])]).transpose()
-    y=np.array([[(zrange[1]-zrange[0])/dim[2]*norm(lv[2])*j/dim[2] for i in range(npts)] for j in range(zrange[1]-zrange[0])]).transpose()
-    
-    fig,ax=plt.subplots(1,1)
-    density_plot=ax.pcolormesh(x,y,z,cmap=cmap,shading='nearest')
-    cbar=fig.colorbar(density_plot)
-    if 'clim' not in args:
-        max_val=max([abs(i) for i in z.flatten()])
-        density_plot.set_clim(vmin=-max_val,vmax=max_val)
-    else:
-        density_plot.set_clim(vmin=args['clim'][0],vmax=args['clim'][1])
-    if 'CHG' in filetype:
-        cbar.set_label('change in electron density / electrons $\AA^{-3}$')
-    if filetype=='LOCPOT':
-        cbar.set_label('change in electron density / electrons $\AA^{-3}$')
-        cbar.ax.yaxis.set_major_formatter(FormatStrFormatter('%+.4f'))
-    atom_pos=[tol]
-    for i in range(1,len(path)):
-        atom_pos.append(atom_pos[-1]+norm(path[i]-path[i-1]))
-        if i==1:
-            atom_pos[i]-=tol
-        if i==len(path)-1:
-            atom_pos[i]-=tol
-    for i in range(len(path_atoms)):
-        for j in range(len(atomtypes)):
-            if path_atoms[i][0]-1 < sum(atomnums[:j+1]):
-                break
-        ax.scatter(atom_pos[i],coord[path_atoms[i][0]-1][2]-zrange[0],color=colors[j],s=sizes[j])
-        
-    if 'contour' in args:
-        if filetype=='LOCPOT':
-            tempvar=parse_LOCPOT(args['contour'][0])[0]
-        else:
-            tempvar=parse_CHGCAR(args['contour'][0])[0]
-        contour_data=np.zeros((npts,zrange[1]-zrange[0]))
-        for i in range(npts):
-            contour_data[i]+=tempvar[int(path_coord[i][0]),int(path_coord[i][1]),zrange[0]:zrange[1]]
-        ax.contour(x,y,contour_data,[args['contour'][1]],colors='black',linestyles='dotted')
-        
-    patches=[]
-    for i in range(len(atomtypes)):
-        patches.append(Patch(color=colors[i],label=atomtypes[i]))
+                tempvar=parse_CHGCAR(args['contour'][0])[0]
+            contour_data=np.zeros((self.npts,self.zrange[1]-self.zrange[0]))
+            for i in range(self.npts):
+                contour_data[i]+=tempvar[int(self.path_coord[i][0]),int(self.path_coord[i][1]),self.zrange[0]:self.zrange[1]]
+            self.ax_main.contour(self.x,self.y,contour_data,[args['contour'][1]],colors='black',linestyles='dotted')
             
-    ax.set(xlabel='position along path / $\AA$')
-    ax.set(ylabel='vertical position / $\AA$')
-    fig.legend(handles=patches)
-    ax.set_aspect('equal')
-    plt.show()
+        patches=[]
+        for i in range(len(self.atomtypes)):
+            patches.append(Patch(color=self.colors[i],label=self.atomtypes[i]))
+                
+        self.ax_main.set(xlabel='position along path / $\AA$')
+        self.ax_main.set(ylabel='vertical position / $\AA$')
+        self.fig_main.legend(handles=patches)
+        self.ax_main.set_aspect('equal')
+        plt.show()
